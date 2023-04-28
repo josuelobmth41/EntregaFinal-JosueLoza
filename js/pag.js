0a226e50bed45bb6d2ba4e32ea45300d6106b14b
josuelobmth <josuelobmth@gmail.com>
2023-04-28 10:36:35 -0300

    falta estructurar

--- js/pag.js
+++ js/pag.js
@@ -1,9 +1,51 @@
-const cartas = ["Mafioso", "Ciudadano", "Policia", "Medico"];
+const elegir = document.querySelector ("#miContenedorDeHTML") 
+
+const cartas = [
+    { id: 1, personaje: "Mafioso" },
+    { id: 2, personaje: "Ciudadano" },
+    { id: 3, personaje: "Policia" },
+    { id: 4, personaje: "Medico" },
+];
+
+const contenedor = document.getElementById("miContenedorDeHTML");
+
+cartas.forEach((carta) => {
+    const cartaHtml = document.createElement("div");
+
+    cartaHtml.classList.add("carta");
+
+    cartaHtml.innerHTML = `
+			<h2>?</h2>
+			<button id="${carta.id}">Elegir</button>
+	`
+    contenedor.appendChild(cartaHtml);
+
+});
 
 function cartasMezclar(array) {
     return array.sort(() => Math.random() - 0.5)
 };
 
-console.log(cartasMezclar(cartas));
+elegir.addEventListener("click", elegirCarta);
+
+function elegirCarta() {
+    
+    const index = Math.floor(Math.random() * cartas.length);
+
+    
+    const cartaSeleccionada = cartas[index];
+
+    
+    console.log(cartaSeleccionada.personaje);
+
+    
+}
+
+
+
+
+
+
+
 
 
--- js/variables.js
+++ js/variables.js
@@ -1,7 +1,3 @@
-const carta = document.querySelector("#carta");
-const cartitas = document.querySelectorAll("#cartitas");
-console.log(cartitas);
 
-cartitas.forEach(cartitas => console.log(cartitas))
 
 
